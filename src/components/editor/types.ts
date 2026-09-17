c984e90730948bb4fa85ff3a2fa75a4a799859d2
snehilshah <snehilshah27@gmail.com>
2026-09-17 12:14:51 +0530

    fix(editor): import StateInline from root markdown-it

--- src/components/editor/types.ts
+++ src/components/editor/types.ts
@@ -4,7 +4,7 @@ import type { SuggestionKeyDownProps, SuggestionProps } from '@tiptap/suggestion
 import type { MarkdownSerializerState } from 'prosemirror-markdown';
 import type { MarkdownStorage } from 'tiptap-markdown';
 import type MarkdownIt from 'markdown-it';
-import type StateInline from 'markdown-it/lib/rules_inline/state_inline';
+import type { StateInline } from 'markdown-it';
 import type { ComponentType } from 'react';
 
 export interface Item {
